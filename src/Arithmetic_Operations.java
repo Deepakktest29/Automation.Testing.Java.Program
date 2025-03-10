e61258bd4794037b5da945372e842536af5f2334
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-10 10:54:50 +0530

    10th March 2025 - Program on All Arithmetic Operations

---
+++ src/Arithmetic_Operations.java
@@ -0,0 +1,12 @@
+public class Arithmetic_Operations {
+    public static void main(String[] args) {
+
+        System.out.println("Arithmetic Operations");
+        System.out.println(2+2);
+        System.out.println(10-3);
+        System.out.println(5*10);
+        System.out.println(8/2);
+        System.out.println(15%2);
+
+    }
+}
