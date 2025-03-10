b85bb006277abf226319f56926a56fa1e570fa7d
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-10 11:01:33 +0530

    10th March 2025 - Added what are types of comments with examples and which are keywords and identifier.

---
+++ src/Comments_Keywords_Identifiers.java
@@ -0,0 +1,12 @@
+public class Comments_Keywords_Identifiers {
+    // This is single line comment in Java
+    /* This is a multi-line comment
+    * In java */
+
+    // Keywords in the programs are public, class, static, and void
+    /* Identifier are Package Name and Comments_Keywords_Identifiers */
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
