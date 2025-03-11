7321c1966eb72732378e6912b4bbd9ce92268759
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-11 12:01:09 +0530

    11th March - Program on different data types

---
+++ src/Data_Types.java
@@ -0,0 +1,23 @@
+public class Data_Types {
+
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 25;
+        int i = 55;
+        long number = 1234567890L;
+        char name = 'A';
+        boolean Is_true = true;
+        float f = 3.14F;
+        double d = 12.43;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println(Is_true);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
