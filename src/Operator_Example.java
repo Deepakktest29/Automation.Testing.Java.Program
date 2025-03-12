1f709c50ce59881a38c21e768277f9ee3554eb76
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-12 15:27:00 +0530

    12th March - Program on Operator

---
+++ src/Operator_Example.java
@@ -0,0 +1,10 @@
+public class Operator_Example {
+    public static void main(String[] args) {
+        System.out.println(10==20);
+        System.out.println(10>=20);
+        System.out.println(10>=20);
+        System.out.println(10>20);
+        System.out.println(10!=20);
+        System.out.println(10>20);
+    }
+}
