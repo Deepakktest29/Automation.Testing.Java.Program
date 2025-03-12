07efcf800df30d7d0c327ba063c2ec86672126eb
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-12 15:39:11 +0530

    12th March - Program on Logical Operator with Example

---
+++ src/Different_OperatorType.java
@@ -0,0 +1,26 @@
+public class Different_OperatorType {
+    public static void main(String[] args) {
+
+        //In this case if both are true then it's true else false
+        System.out.println("AND Operator (&&) : ");
+        System.out.println(true && false);
+        System.out.println(true && true);
+        System.out.println(false && false);
+        System.out.println(false && true);
+        System.out.println("\n");
+
+        //In this case if both are false then it's false else true
+        System.out.println("OR Operator (||) : ");
+        System.out.println(true || false);
+        System.out.println(true || true);
+        System.out.println(false || false);
+        System.out.println(false || true);
+        System.out.println("\n");
+
+        //In this case opposite will come
+        System.out.println("NOT Operator (!) : ");
+        System.out.println(!true);
+        System.out.println(!false);
+
+    }
+}
