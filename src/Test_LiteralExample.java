dfa3d02dab0671affa97fce3c22d9e8e5f77cf1c
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-12 10:56:29 +0530

    12th March - Program on Literal along with explanations and example

---
+++ src/Test_LiteralExample.java
@@ -0,0 +1,12 @@
+public class Test_LiteralExample {
+        public static void main(String[] args) {
+            int age = 25;
+            // Data Type - Integer (Int)
+            // age - Identifier
+            // 25 - Literal (Value assigned to a variable
+
+            System.out.printf("My Age is = %d",age);
+        }
+    }
+}
+}
