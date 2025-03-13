3cf147058cf62264599c1a12fc0b8514e3c81ff1
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 11:42:49 +0530

    13th March - Program on Type Casting - Widening -Implicit

---
+++ src/Type_Casting/Widening_Implicit_Code_001.java
@@ -0,0 +1,11 @@
+package Type_Casting;
+
+public class Widening_Implicit_Code_001 {
+    public static void main(String[] args) {
+
+        System.out.println("Widening - Implicit Type Casting : ");
+        byte b = 25;
+        int a =b;
+        System.out.println(a);
+    }
+}
