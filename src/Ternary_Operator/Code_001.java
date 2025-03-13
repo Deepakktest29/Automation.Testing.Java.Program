a7d190cbb5b955fe505c0234b11d6269e6e53535
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 12:36:23 +0530

    13th March - Ternary Operator - Code001

---
+++ src/Ternary_Operator/Code_001.java
@@ -0,0 +1,10 @@
+package Ternary_Operator;
+
+public class Code_001 {
+    public static void main(String[] args) {
+
+        int age = 10;
+        String result = age >= 18 ? "You can Vote" : "You can't Vote";
+        System.out.println(result);
+    }
+}
