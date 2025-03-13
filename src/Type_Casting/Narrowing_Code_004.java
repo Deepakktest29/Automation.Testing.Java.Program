bb0447655c16dbbfe95c8e1d814566cb60f1810d
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 12:28:14 +0530

    13th March - Narrowing - Explicit Type Casting

---
+++ src/Type_Casting/Narrowing_Code_004.java
@@ -0,0 +1,12 @@
+package Type_Casting;
+
+public class Narrowing_Code_004 {
+    public static void main(String[] args) {
+
+        System.out.println("Narrowing Explicit Type Casting : ");
+        int number = 200;   // Narrowing - Implicit Type Casting
+        byte no = (byte)number;
+        System.out.println(no);
+
+    }
+}
