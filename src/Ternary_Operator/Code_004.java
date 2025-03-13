13a54855908ebba3a19f8d37ab58a8684335d44d
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 13:18:49 +0530

    13th March - Ternary Operator - Code004 - Even/Odd

---
+++ src/Ternary_Operator/Code_004.java
@@ -0,0 +1,11 @@
+package Ternary_Operator;
+
+public class Code_004 {
+    public static void main(String[] args) {
+
+        int number = 10;
+        String result = number % 2==0 ? "It's Even Number" : "It's a Odd Number";
+        System.out.println(result);
+
+    }
+}
