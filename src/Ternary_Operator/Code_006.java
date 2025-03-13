660a6a12582cc419adf5a8720ce925a510a29f26
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 14:45:21 +0530

    13th March - Ternary Operator - Code005 - Age Classification with defined value

---
+++ src/Ternary_Operator/Code_006.java
@@ -0,0 +1,12 @@
+package Ternary_Operator;
+
+public class Code_006 {
+    public static void main(String[] args) {
+
+        // Age Classification with defined value
+        int age = 66;
+        String result = age < 18 ? "Minor" : age > 65 ? "Senior Citizen" : "Adult";
+        System.out.println(result);
+
+    }
+}
