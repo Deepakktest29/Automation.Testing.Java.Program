3da65b59b1f0772a9f712a8143378ead335ef809
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 12:04:21 +0530

    13th March - Widening Explicit Type Casting

---
+++ src/Type_Casting/Widening_Explicit_Code_002.java
@@ -0,0 +1,11 @@
+package Type_Casting;
+
+public class Widening_Explicit_Code_002 {
+    public static void main(String[] args) {
+
+        System.out.println("Widening Explicit Type Casting : ");
+        byte a = 15;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
