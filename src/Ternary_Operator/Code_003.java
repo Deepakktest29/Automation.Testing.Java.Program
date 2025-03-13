98f80ae15c1dfae5f561bffb94c73ae211492fe3
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-13 12:57:01 +0530

    13th March - Ternary Operator - Code003

---
+++ src/Ternary_Operator/Code_003.java
@@ -0,0 +1,19 @@
+package Ternary_Operator;
+
+public class Code_003 {
+    public static void main(String[] args) {
+
+        //Find Maximum number
+        int a = 30;
+        int b = 50;
+
+        System.out.println(Math.max(a,b));
+
+        //Find Maximum Through Ternary Operator
+        int c = 15;
+        int d = 2;
+        String result  = c > d ? "C is greater" : "D is greater";
+        System.out.println(result);
+
+    }
+}
