6ecb380a50b639d24cc6cbcb04ea2efe1ac9e01e
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-20 10:13:41 +0530

    20th March - For Loop Code

---
+++ src/For_Loop/For_Code_006.java
@@ -0,0 +1,14 @@
+package For_Loop;
+
+public class For_Code_006 {
+
+    public static void main(String[] args) {
+
+        for(int i = 10; i > 1; i--) {
+
+            System.out.println(i);
+
+        }
+
+    }
+}
---
+++ src/For_Loop/For_Code_007.java
@@ -0,0 +1,15 @@
+package For_Loop;
+
+public class For_Code_007 {
+
+    public static void main(String[] args) {
+
+
+        for (int i =10; i >=1; i--) {
+
+            System.out.println(i);
+
+        }
+
+    }
+}
