a337d99f0ca09cec7538a50a1bf6569e9036d6d8
Deepakktest29 <Deepakktest29@gmail.com>
2025-03-26 15:09:34 +0530

    26th March - Stringbuffer and StringBuilder

---
+++ src/String_Operations/String_Code_002.java
@@ -0,0 +1,11 @@
+package String_Operations;
+
+public class String_Code_002 {
+
+    public static void main(String[] args) {
+
+        StringBuffer stringBuffer = new StringBuffer("Hello");
+        System.out.println(stringBuffer);
+
+    }
+}
---
+++ src/String_Operations/String_Code_003.java
@@ -0,0 +1,11 @@
+package String_Operations;
+
+public class String_Code_003 {
+
+    public static void main(String[] args) {
+
+        StringBuilder stringbuilder = new StringBuilder("World");
+        System.out.println(stringbuilder);
+
+    }
+}
